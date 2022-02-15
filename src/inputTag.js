13d6c6000198d76c56d065d7d45f95f73b7b71ee
cshaptx4869 <994774638@qq.com>
2022-02-15 11:44:56 +0800

    Fix: 修复数据拷贝时的类型问题

--- src/inputTag.js
+++ src/inputTag.js
@@ -71,7 +71,7 @@
                 var value = this.elem.val().trim();
                 if (!value) return false;
                 if (this.options.beforeCreate && typeof this.options.beforeCreate === 'function') {
-                    var copyData = {...this.options.data};
+                    var copyData = [...this.options.data];
                     var modifiedValue = this.options.beforeCreate(copyData, value);
                     if (typeof modifiedValue == 'string' && modifiedValue) {
                         value = modifiedValue;
@@ -105,7 +105,7 @@
             }
 
             onChange(value, type) {
-                var copyData = {...this.options.data};
+                var copyData = [...this.options.data];
                 this.options.onChange && typeof this.options.onChange === 'function' && this.options.onChange(copyData, value, type);
             }
         }
